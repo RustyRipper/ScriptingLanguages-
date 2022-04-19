ff6d194332dcd3cf618f1f3e6af632297e2b4d2a
Maciej Makara <maciejmakarakontakt@gmail.com>
2022-04-19 13:48:30 +0200

    lab4Ex1 v4 Exceptions

--- lab4/lab4Ex1/MultipleAccumulate.py
+++ lab4/lab4Ex1/MultipleAccumulate.py
@@ -3,21 +3,26 @@ class MultipleAccumulate:
     def __init__(self, data_list, *args):
         self.data_list = data_list
         self.accumulate_functions = args
-        print(self.accumulate_functions)
+        #print(self.accumulate_functions)
 
     def get_data(self):
         dict = {}
         for func in self.accumulate_functions:
-            print(func.__name__)
+            #print(func.__name__)
             dict[func.__name__] = self.__reduce(func, self.data_list)
         return dict
 
-    def __reduce(self, func, iterable, initializer=None):
+    @staticmethod
+    def __reduce(func, iterable, initializer=None):
         it = iter(iterable)
         if initializer is None:
             value = next(it)
         else:
             value = initializer
         for element in it:
-            value = func(value, element)
+            try:
+                value = func(value, element)
+            except ZeroDivisionError:
+                print('division Zero')
+                return 0
         return value
--- lab4/lab4Ex1/ViewerCreator.py
+++ lab4/lab4Ex1/ViewerCreator.py
@@ -10,14 +10,23 @@ class ViewerCreator:
         pass
 
     def create_viewer(self, path):
-        instance: FileViewer = self.__detect_viewer_type(path)(path)
-        return instance
+        try:
+            instance: FileViewer = self.__detect_viewer_type(path)(path)
+            return instance
+        except TypeError or AttributeError:
+            print('cant cast')
+            print(path)
+            print('-------')
+            return None
 
     @staticmethod
     def __detect_viewer_type(path):
+
         ext_of_file = mt.MimeTypes().guess_type(path)[0]
+        if ext_of_file is None:
+            return None
+
         if ext_of_file.__contains__('text/'):
             return TextViewer
         elif ext_of_file.__contains__('image/'):
             return ImageViewer
-
