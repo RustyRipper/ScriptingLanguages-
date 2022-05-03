b0944821ddfcd6ca2d57cf00614029bf43786719
Maciej Makara <maciejmakarakontakt@gmail.com>
2022-05-03 12:46:51 +0200

    lab5 appUpdate

--- lab5/GraphController.py
+++ lab5/GraphController.py
@@ -4,35 +4,49 @@ from py4j.java_gateway import JavaGateway, GatewayParameters
 
 
 class GraphController:
+    graphs = {}
+    actualGraph = None
 
     def __init__(self):
         subprocess.Popen(['java', 'GraphEntryPoint'])
-        self.gateway2 = JavaGateway(gateway_parameters=GatewayParameters(port=25333))
-        self.graph = self.gateway2.entry_point.getGraph()
+        self.gateway2 = JavaGateway()
+
+    def choose_graph(self, graphName):
+        self.actualGraph = self.graphs[graphName]
+
+    def add_graph(self, graphName):
+        self.graphs[graphName] = self.gateway2.entry_point.getNewGraph()
+
+    def print_graphs(self):
+        print(self.graphs)
 
     def add_node(self, label):
-        self.graph.addNode(str(label))
+        self.actualGraph.addNode(str(label))
 
     def remove_node(self, label):
-        self.graph.removeNode(str(label))
+        self.actualGraph.removeNode(str(label))
 
     def add_edge(self, label_source, label_dest, weight):
-        self.graph.addEdge(str(label_source), str(label_dest), weight)
+        self.actualGraph.addEdge(str(label_source), str(label_dest), weight)
 
     def remove_edge(self, label_source, label_dest):
-        self.graph.removeEdge(str(label_source), str(label_dest))
+        self.actualGraph.removeEdge(str(label_source), str(label_dest))
 
     def bfs(self, root):
-        print(self.graph.BFS(str(root)))
+        print(self.actualGraph.BFS(str(root)))
 
     def dfs(self, root):
-        print(self.graph.DFS(str(root)))
+        print(self.actualGraph.DFS(str(root)))
+
+    def change_node_label(self, old_label, new_label):
+        self.actualGraph.modifyNodeLabel(str(old_label), str(new_label))
+
+    def change_edge_weight(self, source_label, dest_label, weight):
+        self.actualGraph.modifyWeightEdge(str(source_label), str(dest_label), weight)
 
     def print_graph(self):
-        print(self.graph.toString())
+        print(self.actualGraph.toString())
 
-    @staticmethod
-    def stop_graph():
-        jg = JavaGateway()
-        jg.close()
-        jg.shutdown()
+    def stop_graph(self):
+        self.gateway2.close()
+        self.gateway2.shutdown()
--- lab5/app.py
+++ lab5/app.py
@@ -1,58 +1,92 @@
 from GraphController import GraphController
-
-graph = GraphController()
+import time
+graph_controller = GraphController()
 
 flag = True
 
 
-def upload_data():
-    graph.add_node('jeden')
-    graph.add_node('dwa')
-    graph.add_node('trzy')
-    graph.add_node('cztery')
+def menu_graph():
+    flag2 = True
+    while flag2:
+        print("---------Graph----------")
+        print("0.Back")
+        print("1.Print Graph")
+        print("2.Add node")
+        print("3.Remove node")
+        print("4.Add edge")
+        print("5.Remove edge")
+        print("6.DFS")
+        print("7.BFS")
+        print("8.Upload data")
+        print("9.Change node label")
+        print("10.Change edge weight")
 
-    graph.add_edge("jeden", "dwa", 2)
-    graph.add_edge("dwa", "trzy", 5)
-    graph.add_edge("cztery", "jeden", 5)
-    graph.add_edge("jeden", "dwa", 3)
-    graph.add_edge("jeden", "cztery", 3)
+        try:
+            choice2 = int(input("Choose option: "))
+        except ValueError:
+            print("bad parametr")
+            choice2 = -1
+
+        if choice2 == 1:
+            graph_controller.print_graph()
+        elif choice2 == 2:
+            graph_controller.add_node(input("Set label: "))
+        elif choice2 == 3:
+            graph_controller.remove_node(input("Insert label(node) to remove: "))
+        elif choice2 == 4:
+            graph_controller.add_edge(input("Insert label(node source): "), input("Insert label(node dest): "),
+                                      int(input("Insert weight of edge: ")))
+        elif choice2 == 5:
+            graph_controller.remove_edge(input("Insert label(node source): "), input("Insert label(node dest): "))
+        elif choice2 == 6:
+            graph_controller.dfs(input("Insert root-label: "))
+        elif choice2 == 7:
+            graph_controller.bfs(input("Insert root-label: "))
+        elif choice2 == 8:
+            upload_data()
+        elif choice2 == 9:
+            graph_controller.change_node_label(input("Insert old label: "), input("Insert  new label: "))
+        elif choice2 == 10:
+            graph_controller.change_edge_weight(input("Insert label(node source): "), input("Insert label(node dest): "), int(input("new weight: ")))
+        else:
+            flag2 = False
+
+
+def upload_data():
+    graph_controller.add_node('jeden')
+    graph_controller.add_node('dwa')
+    graph_controller.add_node('trzy')
+    graph_controller.add_node('cztery')
+
+    graph_controller.add_edge("jeden", "dwa", 2)
+    graph_controller.add_edge("dwa", "trzy", 5)
+    graph_controller.add_edge("cztery", "jeden", 5)
+    graph_controller.add_edge("jeden", "dwa", 3)
+    graph_controller.add_edge("jeden", "cztery", 3)
 
 
 while flag:
     print("---------Menu----------")
-    print("1.Print Graph")
-    print("2.Add node")
-    print("3.Remove node")
-    print("4.Add edge")
-    print("5.Remove edge")
-    print("6.DFS")
-    print("7.BFS")
-    print("8.Upload data")
+    print("0.Exit")
+    print("1.Create graph")
+    print("2.Choose graph")
+    print("3.Print graphs")
+
     try:
-        choice = int(input("Choose option:"))
+        choice = int(input("Choose option: "))
     except ValueError:
         print("bad parametr")
-        choice = 0
+        choice = -1
     if choice == 0:
-        print("try again")
-    elif choice == 1:
-        graph.print_graph()
-    elif choice == 2:
-        graph.add_node(input("Set label"))
-    elif choice == 3:
-        graph.remove_node(input("Insert label(node) to remove"))
-    elif choice == 4:
-        graph.add_edge(input("Insert label(node source)"), input("Insert label(node dest)"),
-                       int(input("Insert weight of edge")))
-    elif choice == 5:
-        graph.remove_edge(input("Insert label(node source)"), input("Insert label(node dest)"))
-    elif choice == 6:
-        graph.dfs(input("Insert root-label"))
-    elif choice == 7:
-        graph.bfs(input("Insert root-label"))
-    elif choice == 8:
-        upload_data()
-    else:
-        graph.stop_graph()
         flag = False
-
+    if choice == 1:
+        graph_controller.add_graph(input("Set graphName: "))
+    elif choice == 2:
+        graph_controller.choose_graph(input("Choose graph: "))
+        menu_graph()
+    elif choice == 3:
+        graph_controller.print_graphs()
+    else:
+        graph_controller.stop_graph()
+        time.sleep(2.5)
+        flag = False
